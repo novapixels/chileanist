31a055e5286ff9bad858ca3961c11c714fbfb102
monica randriamialy <mo@novapixels.net>
2021-12-11 15:59:08 +0100

    Generated Expression controller, model and view, with index and show methods

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_11_142233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expressions", force: :cascade do |t|
+    t.string "sentence"
+    t.string "definition"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+Expression.destroy_all
+
+puts 'Creating expressions'
+Expression.create(
+  sentence: 'Como piojo',
+  definition: 'Estar borracho'
+)
+Expression.create(
+  sentence: 'Echar la yegua',
+  definition: 'Cansarse o que te entra un poco de sueño, sobre todo después de comer'
+)
+Expression.create(
+  sentence: 'Pasarlo chancho',
+  definition: 'Pasarlo muy bien'
+)
+Expression.create(
+  sentence: 'Morir pollo',
+  definition: 'Callarse'
+)
