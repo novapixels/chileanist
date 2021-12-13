aef1570c0d52343b31ec8b91dadf35e81ac7f2dc
monica randriamialy <mo@novapixels.net>
2021-12-13 21:29:15 +0100

    Seeded +100 Chilean expressions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,8 +6,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 puts 'Cleaning database'
 Expression.destroy_all
+
+puts 'parsing CSV seeds'
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'expresiones-cl.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+
+puts 'Looping through the parsed data'
+csv.each do |row|
+  Expression.create(
+    sentence: "#{row['Sentence']}",
+    definition: "#{row['Definition']}"
+  )
+end
+puts 'All expressions created'
 
 puts 'Creating expressions'
 Expression.create(
