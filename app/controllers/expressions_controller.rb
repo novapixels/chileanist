0361231472911b72037fc1ed21ee8f7c9c4e0b65
monica randriamialy <mo@novapixels.net>
2022-06-07 16:23:06 +0200

    last push

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -5,11 +5,37 @@ class ExpressionsController < ApplicationController
     init_num = Expression.first.id
     last_num = Expression.last.id
     generated_expression_id = rand(init_num..last_num)
+    # stores a random expretion into a variable
+    @expressionsample = Expression.find_by_id(generated_expression_id)
 
-    @expressionsample = Expression.find(generated_expression_id)
+    button_click = params[generated_expression_id]
+
+    if button_click.present?
+      "#{@expressionsample[:sentence]} #{@expressionsample[:definition]} "
+    end
+
   end
 
   def show
     @expression = Expression.find(params[:id])
   end
+
+  def update
+    respond_to do |format|
+        format.html do
+          render(@expression)
+        end
+
+        format.json { render(:show, sentence: @expression)}
+     end
+  end
+
+  # private
+
+  # def expression_params
+  #   params.require(:expression).permit(
+  #     :sentence,
+  #     :definition
+  #   )
+  # end
 end
