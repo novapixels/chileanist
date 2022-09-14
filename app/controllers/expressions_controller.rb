4dca737e2f83cafd7f87e7a46bdcb1ddb30d2600
monica randriamialy <mo@novapixels.net>
2022-09-14 12:35:36 -0300

    Random expression page works with hotwire frame now

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -17,7 +17,15 @@ class ExpressionsController < ApplicationController
   end
 
   def show
-    @expression = Expression.find(params[:id])
+    # @expression = Expression.find(params[:id])
+    @expressions = Expression.all
+    # generate a number between first and last expresion id
+    init_num = Expression.first.id
+    last_num = Expression.last.id
+    generated_expression_id = rand(init_num..last_num)
+    raise
+    # stores a random expretion into a variable
+    @expressionsample = Expression.find_by_id(generated_expression_id)
   end
 
   def update
